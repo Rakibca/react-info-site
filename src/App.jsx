5f4a5feb7cd1dcf3825e9871f01ab7eedac544cf
Rakibul Islam <rakib.ca@gmail.com>
2024-05-02 09:43:50 -0600

    minor updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 function App() {
   return (
     <div>
-      <img src="./react-logo.png" alt="react-logo" width="40px" />
+      <img src="./react-logo.png" alt="React Logo" width="40px" />
       <h1>Fun facts about React</h1>
       <ul>
         <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100K stars oon Github</li>
+        <li>Was originally created by Jordan Walker</li>
+        <li>Has well over 100K stars oon GitHub</li>
         <li>Is maintained by Facebook</li>
         <li>Powers thousands of enterprise apps, including mobile apps</li>
       </ul>

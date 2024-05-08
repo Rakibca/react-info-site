fce74b66f3a8f18786d34e7e8ef0d6be65a42ddc
Rakibul Islam <rakib.ca@gmail.com>
2024-05-08 10:13:27 -0600

    custom components part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 function App() {
   return (
-    <div>
-      <img src="./react-logo.png" alt="React Logo" width="40px" />
+    <>
+      <header>
+        <nav>
+          <img src="./react-logo.png" alt="React Logo" width="40px" />
+        </nav>
+      </header>
       <h1>Fun facts about React</h1>
       <ul>
         <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walker</li>
+        <li>Was originally created by Jordan Walke</li>
         <li>Has well over 100K stars oon GitHub</li>
         <li>Is maintained by Facebook</li>
         <li>Powers thousands of enterprise apps, including mobile apps</li>
       </ul>
-    </div>
+      <footer>
+        <small>© 2024 Islam development. All rights reserved.</small>
+      </footer>
+    </>
   );
 }
 
